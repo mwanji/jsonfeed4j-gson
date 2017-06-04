cea6c2832163702d8ffc5ba6cd0302ea9eb85a3e
Moandji Ezana <mwanji@gmail.com>
2017-06-04 08:55:01 +0200

    Enable running JUnit 5 in Eclipse Neon

---
+++ src/test/java/jsonfeed4j/AllTestsWithJunit4TestSuite.java
@@ -0,0 +1,11 @@
+package jsonfeed4j;
+
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+@SelectPackages({ "jsonfeed4j", "jsondeed4j.validation" })
+public class AllTestsWithJunit4TestSuite {
+
+}
